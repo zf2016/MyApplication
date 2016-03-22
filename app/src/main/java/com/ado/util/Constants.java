8b45af0d7e5efe7e285116d6da26cc39761c7735
zf2016 <308984454@qq.com>
2016-03-22 10:08:43 +0800

    Rejected commit: Default

---
+++ app/src/main/java/com/ado/util/Constants.java
@@ -0,0 +1,14 @@
+package com.ado.util;
+
+/**
+ * 常量
+ *
+ * @author zf-win10
+ *         created at 2016/3/21 16:51
+ */
+public class Constants {
+
+    public static final boolean LOG_DEBUG = true;
+
+
+}
